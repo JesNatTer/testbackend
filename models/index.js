63987c12a4e592222185983ea6f9b05572cd8564
Jesse Terblanche <82204857+JesNatTer@users.noreply.github.com>
2023-05-29 06:18:13 +0200

    Update index.js

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const db = {};
 console.log(process.env.DIALECT)
 
 
-const sequelize = new Sequelize(config.url, {
+const sequelize = new Sequelize(process.env.URL, {
   // host: process.env.HOST,
   // dialect: process.env.DIALECT,
   pool : {
