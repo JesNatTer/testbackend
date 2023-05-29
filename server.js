dab351632eb03361884ed090623f05b2cec5d35c
Jesse Terblanche <82204857+JesNatTer@users.noreply.github.com>
2023-05-29 05:30:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv')
-dotenv.config({path:`${__dirname}/.env`});
+require('dotenv')config({path:`${__dirname}/.env`});
+// dotenv.config({path:`${__dirname}/.env`});
 // dotenv.config({path:`${__dirname}/etc/secrets/.env`});
 const express = require("express");
 const cookieParser = require('cookie-parser')
