97037b281875da6954b667fe035b2bb63c83c847
Jesse Terblanche <82204857+JesNatTer@users.noreply.github.com>
2023-05-29 06:03:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-require('dotenv')config();
-// dotenv.config({path:`${__dirname}/.env`});
+const dotenv = require('dotenv');
+dotenv.config({path:`${__dirname}/.env`});
 // dotenv.config({path:`${__dirname}/etc/secrets/.env`});
-// const express = require("express");
-// const cookieParser = require('cookie-parser')
-// dotenv.config({path:`${__dirname}/.env`});
-// const app = express();
+const express = require("express");
+const cookieParser = require('cookie-parser')
+const app = express();
 console.log(process.env)
-// const routes = require('./routes/routes')
+const routes = require('./routes/routes')
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// app.use(express.json())
-// app.use(routes)
-// app.use(cookieParser)
-// app.get('/', (req, res) => {
-//     res.send("Hello World!")
-// })
+app.use(express.json())
+app.use(routes)
+app.use(cookieParser)
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
 
-// app.use('/api/v1/', routes)
+app.use('/api/v1/', routes)
 
-// app.listen(port, () => console.log(process.env), console.log(`app listening on port ${port}`))
+app.listen(port, () => console.log(process.env), console.log(`app listening on port ${port}`))
